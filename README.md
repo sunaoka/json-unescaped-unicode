ec04d2da19bdfe0c29f18de12c09d3f03f5bd8d8
sunaoka <sunaoka@pocari.org>
2023-05-29 10:26:12 +0900

    Initial release.

---
+++ README.md
@@ -0,0 +1,11 @@
+# json-unescaped-unicode
+
+## Features
+
+## Known Issues
+
+## Release Notes
+
+### 0.0.1
+
+Initial release.
---
+++ src/extension.ts
@@ -0,0 +1,33 @@
+import * as vscode from 'vscode';
+
+const getIndentCharacters = () => {
+	if (vscode.window.activeTextEditor?.options.insertSpaces) {
+		return ' '.repeat(vscode.window.activeTextEditor.options.tabSize as number);
+	} else {
+		return '\t';
+	}
+};
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let disposable = vscode.commands.registerCommand('json-unescaped-unicode.run', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+
+		const document = editor.document;
+
+		const json = JSON.parse(document.getText());
+
+		editor?.edit(edit => {
+			const range = new vscode.Range(document.lineAt(0).range.start, document.lineAt(document.lineCount - 1).range.end);
+			edit.replace(range, JSON.stringify(json, null, getIndentCharacters()));
+		}).then(() => {
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
