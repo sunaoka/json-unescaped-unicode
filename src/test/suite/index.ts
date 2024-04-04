f9c62e1ca37dde46573761b1e3737750ca849b30
sunaoka <sunaoka@pocari.org>
2024-04-04 12:17:31 +0900

    Update dependencies

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
-import * as Mocha from 'mocha';
-import * as glob from 'glob';
+import Mocha from 'mocha';
+import { glob } from 'glob';
 
 export function run(): Promise<void> {
 	// Create the mocha test
@@ -12,28 +12,28 @@ export function run(): Promise<void> {
 
 	const testsRoot = path.resolve(__dirname, '..');
 
-	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
-
+	return new Promise((resolve, reject) => {
+		glob('**/**.test.js', { cwd: testsRoot }).then(files => {
 			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+			files.forEach(file => mocha.addFile(path.resolve(testsRoot, file)));
 
 			try {
 				// Run the mocha test
 				mocha.run(failures => {
 					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
+						reject(new Error(`${failures} tests failed.`));
 					} else {
-						c();
+						resolve();
 					}
 				});
+
 			} catch (err) {
 				console.error(err);
-				e(err);
+				reject(err);
 			}
+
+		}).catch(err => {
+			return reject(err);
 		});
 	});
 }
